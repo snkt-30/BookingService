0020f31473d556a18272e306ba77cb9b44453775
Sanket Khonde <sanketkhonde30@gmail.com>
2024-12-30 18:27:28 +0530

    Added migration to add the new column

--- src/models/booking.js
+++ src/models/booking.js
@@ -27,6 +27,16 @@ module.exports = (sequelize, DataTypes) => {
         values: ["InProcess", "Booked", "Cancelled"],
         defaultValue: "Inprocess",
       },
+      noOfSeats: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      totalCost: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
     },
     {
       sequelize,
